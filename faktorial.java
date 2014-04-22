30e59f07dd6572ec761661f977c1c0ab6915bf65
Rahmimn <rahmimn@gmail.com>
2014-04-22 17:51:58 +0700

    Create faktorial.java

---
+++ faktorial.java
@@ -0,0 +1,13 @@
+public class faktorial {
+    public static void main(String[] args) {
+        int jum,ang,a,b,tot;
+        jum=7;
+        ang=5;
+        a=1;
+        b=0;
+        for(int i=1;i<=jum;i++){
+            a=i*a;
+        }
+        System.out.println(a);
+    }
+}
